9db6d59734882187f94e997488e34cf9d4c18294
parksubeom <a01081090731@gmail.com>
2023-02-18 00:41:46 +0900

    feat:블로그 컨셉컬러 초이스

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,13 +5,13 @@ export const Form = () => {
     return (
         <section className ="Form__container">
             <form>
-                <div>
+                <div className='search_form'>
                     <label htmlFor="search">Search</label>
                     <input
                      type="text"
-                     name="search"
-                     value=""
+                     name="search"                    
                      autoComplete='off'
+                     id ="search_input"
                      />
                 </div>
             </form>
